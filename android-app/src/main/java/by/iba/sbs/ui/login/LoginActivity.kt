12f683a7ae4c3c88a1fda225c9fa13b7316ce794
Dobry <zhigar@gmail.com>
2020-03-29 20:37:38 +0300

    Login activity added

---
+++ android-app/src/main/java/by/iba/sbs/ui/login/LoginActivity.kt
@@ -0,0 +1,20 @@
+package by.iba.sbs.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.iba.sbs.R
+import by.iba.sbs.ui.ui.login.LoginFragment
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, LoginFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ android-app/src/main/java/by/iba/sbs/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package by.iba.sbs.ui.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
