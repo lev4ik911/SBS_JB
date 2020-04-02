5969461b3b31529d97abced58f3dc5e63cd6a176
Dobry <zhigar@gmail.com>
2020-04-02 16:56:15 +0300

    login activity (WIP)

--- android-app/src/main/java/by/iba/sbs/di/AppModule.kt
+++ android-app/src/main/java/by/iba/sbs/di/AppModule.kt
@@ -1,6 +1,7 @@
 package by.iba.sbs.di
 
 import by.iba.sbs.ui.login.LoginViewModel
+import by.iba.sbs.ui.login.RegisterViewModel
 import by.iba.sbs.ui.login.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,6 +9,6 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel { SplashViewModel() }
     viewModel { LoginViewModel() }
-
+    viewModel { RegisterViewModel() }
     //viewModel { PostDetailsViewModel(userPostUseCase = get(), commentsUseCase = get()) }
 }
