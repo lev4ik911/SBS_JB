c6db056355b4d510cce7445c677c0c79c8fa506f
Dobry <zhigar@gmail.com>
2020-05-18 23:43:19 +0300

    SQLDelight: common code for creation DB added

--- mpp-library/src/commonMain/kotlin/by/iba/sbs/library/repository/GuidelineRepository.kt
+++ mpp-library/src/commonMain/kotlin/by/iba/sbs/library/repository/GuidelineRepository.kt
@@ -1,8 +1,11 @@
 package by.iba.sbs.library.repository
 
+import by.iba.sbs.db.SBSDB
 import by.iba.sbs.library.data.remote.Client
 import by.iba.sbs.library.data.remote.Response
 import by.iba.sbs.library.model.Guideline
+import by.iba.sbs.library.service.LocalSettings
+import com.russhwolf.settings.Settings
 import dev.icerock.moko.mvvm.livedata.LiveData
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.UnstableDefault
@@ -10,8 +13,18 @@ import kotlinx.serialization.UnstableDefault
 interface IGuidelineRepository{
     suspend fun getAllGuidelines(): LiveData<Response<List<Guideline>>>
 }
+
+expect fun createDb(): SBSDB
+
 @ImplicitReflectionSerializer
-class GuidelineRepository @UnstableDefault constructor(private val remote: Client):IGuidelineRepository{
+class GuidelineRepository @UnstableDefault constructor(private val settings: Settings) :
+    IGuidelineRepository {
+    private val localStorage: LocalSettings by lazy { LocalSettings(settings) }
+
+    @UnstableDefault
+    private val galwayBusApi = Client(settings)
+    private val sbsDb = createDb()
+    private val galwayBusQueries = sbsDb.guidelinesEntityQueries
     override suspend fun getAllGuidelines(): LiveData<Response<List<Guideline>>> {
         TODO("Not yet implemented")
     }
