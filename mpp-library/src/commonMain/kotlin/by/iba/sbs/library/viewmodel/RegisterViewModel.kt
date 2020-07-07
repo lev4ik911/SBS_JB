a3f50132acbd02efb8a9bcd3ae38177caa882b52
Nikolai Andronov <nikolai_andronov@goodyear.com>
2020-07-07 10:29:32 -0400

    add validation for registration

--- mpp-library/src/commonMain/kotlin/by/iba/sbs/library/viewmodel/RegisterViewModel.kt
+++ mpp-library/src/commonMain/kotlin/by/iba/sbs/library/viewmodel/RegisterViewModel.kt
@@ -15,13 +15,20 @@ import kotlinx.coroutines.launch
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.UnstableDefault
 
+enum class ValidationErrors(var index: Int) {
+    LOGIN_IS_EMPTY(1),
+    INVALID_EMAIL(2),
+    PASSWORD_IS_TOO_SMALL(3),
+    PASSWORD_HAS_INCORRECT_SYMBOLS(4),
+    PASSWORD_MISMATCH(5)
+}
+
 class RegisterViewModel(
     settings: Settings,
     override val eventsDispatcher: EventsDispatcher<EventsListener>,
     systemInfo: SystemInformation
 ) : ViewModelExt(settings),
     EventsDispatcherOwner<RegisterViewModel.EventsListener> {
-
 
     val appVersion: String = systemInfo.getAppVersion()
 
@@ -42,35 +49,70 @@ class RegisterViewModel(
     val email = MutableLiveData("")
     val password = MutableLiveData("")
     val passwordConfirm = MutableLiveData("")
+    var errorList = mutableListOf<ValidationErrors>()
 
     @UnstableDefault
     @ImplicitReflectionSerializer
     fun onRegisterButtonClick() {
-        loading.value = true
-        viewModelScope.launch {
-            try {
-                val response =
-                    authRepository.register(RegisterData(login.value, email.value, password.value))
-                if (response.isSuccess && response.isNotEmpty) {
-                    response.data?.let {
-                        usersQueries.addUser(it.id, it.name, it.email)
-                        localStorage.userId = it.id
-                        eventsDispatcher.dispatchEvent { routeToLoginScreen() }
+        validation()
+        if (errorList.isEmpty()) {
+            loading.value = true
+            viewModelScope.launch {
+                try {
+                    val response =
+                        authRepository.register(
+                            RegisterData(
+                                login.value,
+                                email.value,
+                                password.value
+                            )
+                        )
+                    if (response.isSuccess && response.isNotEmpty) {
+                        response.data?.let {
+                            usersQueries.addUser(it.id, it.name, it.email)
+                            localStorage.userId = it.id
+                            eventsDispatcher.dispatchEvent { routeToLoginScreen() }
+                        }
                     }
-                }
-                loading.postValue(false)
-            } catch (e: Exception) {
-                eventsDispatcher.dispatchEvent {
-                    showToast(
-                        ToastMessage(e.message.toString(), MessageType.ERROR)
-                    )
+                    loading.postValue(false)
+                } catch (e: Exception) {
+                    eventsDispatcher.dispatchEvent {
+                        showToast(
+                            ToastMessage(e.message.toString(), MessageType.ERROR)
+                        )
+                    }
                 }
             }
         }
+        else {
+            eventsDispatcher.dispatchEvent { showErrors() }
+        }
+
     }
+
+    fun validation() {
+        errorList.clear()
+        if (login.value.isEmpty()) {
+            errorList.add(ValidationErrors.LOGIN_IS_EMPTY)
+        }
+        if (!email.value.contains("@")) {
+            errorList.add(ValidationErrors.INVALID_EMAIL)
+        }
+        if (password.value.length < 5) {
+            errorList.add(ValidationErrors.PASSWORD_IS_TOO_SMALL)
+        }
+        if (password.value.contains(" ")) {
+            errorList.add(ValidationErrors.PASSWORD_HAS_INCORRECT_SYMBOLS)
+        }
+        if (!passwordConfirm.value.equals(password.value)){
+            errorList.add(ValidationErrors.PASSWORD_MISMATCH)
+        }
+    }
+
 
     interface EventsListener {
         fun routeToLoginScreen()
         fun showToast(msg: ToastMessage)
+        fun showErrors()
     }
 }
